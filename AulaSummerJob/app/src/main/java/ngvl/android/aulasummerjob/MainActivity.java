adbe630d73bb3cf162f24110b2251cb3cfdfaf13
rubemmoura <rfmj2@cin.ufpe.br>
2016-01-18 09:26:24 -0300

    Botão do LoginFacebook adicionado. Solicita permissão em sua conta do facebook pelo app. Retorna campos que ainda serão tratados.

--- AulaSummerJob/app/src/main/java/ngvl/android/aulasummerjob/MainActivity.java
+++ AulaSummerJob/app/src/main/java/ngvl/android/aulasummerjob/MainActivity.java
@@ -1,21 +1,36 @@
 package ngvl.android.aulasummerjob;
 
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity{
 
-
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +38,21 @@ public class MainActivity extends AppCompatActivity{
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
+        info = (TextView)findViewById(R.id.info);
+        callbackManager = CallbackManager.Factory.create();
+
         final EditText edtNome = (EditText)findViewById(R.id.edtNome);
         final EditText edtNome2 = (EditText)findViewById(R.id.editText);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
 
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
 
         Button btn = (Button)findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
-                        "Mensagem! " + edtNome.getText().toString(),
+                        "Logado! " + edtNome.getText().toString(),
                         Toast.LENGTH_SHORT).show();
 
                 Intent it = new Intent(MainActivity.this, Tela2Activity.class);
@@ -40,6 +60,43 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User Name: " + "\n" + "Auth Token: ");
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.v("LoginActivity", response.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
